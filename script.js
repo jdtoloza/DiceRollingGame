ea464142e0a45ee62fe8eb994ddf49e345dbd5f6
Josh Toloza <90005135+jdtoloza@users.noreply.github.com>
2022-08-23 02:18:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -39,11 +39,11 @@ const switchPlayer= function(){
 // Rolling dice functionality
 btnRoll.addEventListener('click', function(){
     if(playing){
-
         // 1. Generating a random dice roll. Math.trunc returns the integer part of a number by removing any fractional digits. 
         const dice = Math.trunc(Math.random() * 6) + 1
         // 2. Display dice
         diceEL.classList.remove('hidden')
+        // adding all the dice rolls to the image 
         diceEL.src = `dice-${dice}.png`;
         // 3. Check for rolled 1: if true, switch to next player. 
         if(dice !== 1){
@@ -54,7 +54,6 @@ btnRoll.addEventListener('click', function(){
         } else {
             // switch to the next player
             switchPlayer()
-            
         }
 }
 });
@@ -68,7 +67,7 @@ btnHold.addEventListener('click', function(){
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
         
         // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 50) {
         diceEL.classList.add('hidden');
         // Finish the game
         
@@ -104,8 +103,6 @@ function newGame() {
         player1EL.classList.remove('player--active')
         document.querySelector('.winning-announcement-window-background').classList.remove('active-winner');
         document.querySelector('.winning-announcement-window').classList.remove('active-winner')
-
-    
         
     }
 
